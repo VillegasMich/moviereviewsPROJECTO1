99ff6eaad18dca60f0435d27b75b808f0407ace2
Manuel Villegas <manuel.villegas.michel@gmail.com>
2023-08-03 11:45:16 -0500

    Starting part 2

--- movie/views.py
+++ movie/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 def home(request):
     # return HttpResponse('<h1>Welcome to home page</h1>')
     return render(request, 'home.html', {'name': 'Manuel Villegas Michel'})
+
+def about(request):
+    return render(request, 'about.html')
