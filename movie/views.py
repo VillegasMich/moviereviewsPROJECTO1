567675fb8203af16ce1c42c94ded5adf48ed177e
Manuel Villegas <manuel.villegas.michel@gmail.com>
2023-08-03 22:11:19 -0500

    up to part 6.4

--- movie/views.py
+++ movie/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 def home(request):
     # return HttpResponse('<h1>Welcome to home page</h1>')
-    return render(request, 'home.html', {'name': 'Manuel Villegas Michel'})
+    # return render(request, 'home.html', {'name': 'Manuel Villegas Michel'})
+    searchTerm = request.GET.get('searchMovie')
+    movies = Movie.objects.all()
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
 
 def about(request):
     return render(request, 'about.html')
